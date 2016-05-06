4beaa5b61414f161f992fdbbf02c953db0b5c6fc
misoton665 <misoton998@hotmail.co.jp>
2016-05-06 11:48:26 +0900

    Remove some comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,11 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import {window, commands, workspace, InputBoxOptions, ExtensionContext} from 'vscode';
 
 declare function require(x: string): any;
 
 var exec = require('child_process').exec;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
 
   console.log('Congratulations, your extension "bellterm" is now active!');
@@ -44,24 +41,6 @@ export function activate(context: ExtensionContext) {
   context.subscriptions.push(promptShowing);
   context.subscriptions.push(consoleCreation);
   context.subscriptions.push(consoleChannel);
-}
-
-var showStdout = compose3(extractStdout, window.showInformationMessage);
-
-function extractStdout(err: string, stdout: string, stderr: string) {
-  return stdout;
-}
-
-function compose1<A, B, C>(f: (A) => B, g: (B) => C) {
-  return function(arg: A) {
-    return g(f(arg));
-  }
-}
-
-function compose3<A1, A2, A3, B, C>(f: (A1, A2, A3) => B, g: (B) => C) {
-  return function(arg1: A1, arg2: A2, arg3: A3) {
-    return g(f(arg1, arg2, arg3));
-  }
 }
 
 export function deactive() {
