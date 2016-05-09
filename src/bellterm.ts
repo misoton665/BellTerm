3dd284c3d32806487f8f6f117aba2e46fd379e6c
misoton665 <misoton998@hotmail.co.jp>
2016-05-09 13:05:09 +0900

    Add async functions for checking accessable and running command

--- src/bellterm.ts
+++ src/bellterm.ts
@@ -1,13 +1,42 @@
 declare function require(x: string): any;
 
 export type CommandCallback = (error: Error, stdout: Buffer, stderr: Buffer) => void;
+export type CommandResultTuple = [Error, Buffer, Buffer];
 
 export class BellTerm {
+  // for command running
   private cp = require('child_process');
+  
+  // for shell command file access and chmod
+  private fs = require('fs');
   private dirroot: string;
   
   constructor(dirroot: string = "/") {
     this.dirroot = dirroot;
+  }
+  
+  private accessFileAsync(filePath: string, mode: any): Promise<boolean> {
+    return new Promise(function(resolve, reject) {
+      this.fs.access(filePath, mode, function(err){
+        if(err) {
+          reject(err);
+        } else {
+          resolve(null);
+        }
+      });
+    });
+  }
+  
+  private runCommandAsync(command: string, option: {}): Promise<CommandResultTuple> {
+    return new Promise(function(resolve, reject) {
+      this.cp.exec(command, option, function(error, stdout, stderr) {
+        if(error) {
+          reject(error);
+        } else {
+          resolve([error, stdout, stderr]);
+        }
+      });
+    });
   }
   
   public getDefaultCommandFile(): string {
